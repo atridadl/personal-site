ee0e838a1ebd93900b101acc22d983d3ff85ffa7
Atridad Lahutton <atridad@icloud.com>
2022-02-26 02:42:33 -0700

    Updated links

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -16,7 +16,8 @@ import VueFeather from 'vue-feather';
     <div class="flex flex-wrap justify-center items-center space-x-3 m-6">
       <a class="group" href="https://gethub.ca/atridad" target="_blank"><vue-feather class="group-hover:text-pink-600" type="git-pull-request"></vue-feather></a>
       <a class="group" href="mailto:atridad@icloud.com" target="_blank"><vue-feather class="group-hover:text-pink-600" type="mail"></vue-feather></a>
-      <a class="group" href="https://mast.atridad.dev/@atridad" target="_blank"><vue-feather class="group-hover:text-pink-600" type="hash"></vue-feather></a>
+      <a class="group" href="https://twitter.com/atridadl" target="_blank"><vue-feather class="group-hover:text-pink-600" type="twitter"></vue-feather></a>
+      <a class="group" href="https://www.instagram.com/atridadl/" target="_blank"><vue-feather class="group-hover:text-pink-600" type="instagram"></vue-feather></a>
     </div>
 
     <h3 class="text-2xl font-black text-center m-6">Freelance Software Engineer, Sysadmin, and Certified Nerd</h3>
