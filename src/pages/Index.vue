e804649e3f6aeab557eb50e5941b1ba923b17002
Atridad Lahutton <88056492+atridadl@users.noreply.github.com>
2022-03-02 16:28:54 -0700

    Accessability changes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,15 +9,31 @@ import VueFeather from 'vue-feather';
     <h2 class="text-xl font-black text-center">(he/him)</h2>
 
     <img
-      class="rounded-lg mx-auto my-6"
+      class="rounded-lg mx-auto my-6 max-w-[250px] w-1/2"
       src="/src/assets/logo.webp"
+      alt="A cartoon drawing of my face."
     />
 
     <div class="flex flex-wrap justify-center items-center space-x-3 m-6">
-      <a class="group" href="https://gethub.ca/atridad" target="_blank"><vue-feather class="group-hover:text-pink-600" type="git-pull-request"></vue-feather></a>
-      <a class="group" href="mailto:atridad@icloud.com" target="_blank"><vue-feather class="group-hover:text-pink-600" type="mail"></vue-feather></a>
-      <a class="group" href="https://twitter.com/atridadl" target="_blank"><vue-feather class="group-hover:text-pink-600" type="twitter"></vue-feather></a>
-      <a class="group" href="https://www.instagram.com/atridadl/" target="_blank"><vue-feather class="group-hover:text-pink-600" type="instagram"></vue-feather></a>
+      <a class="text-center no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-pink-600 hover:text-pink-600 mt-4 sm:mt-0" href="https://github.com/atridadl" target="_blank">
+        <vue-feather type="github"></vue-feather>
+        <div>Github</div>
+      </a>
+
+      <a class="text-center no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-pink-600 hover:text-pink-600 mt-4 sm:mt-0" href="mailto:atridad@icloud.com" target="_blank">
+        <vue-feather class="group-hover:text-pink-600" type="mail"></vue-feather>
+        <div>Email</div>
+      </a>
+
+      <a class="text-center no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-pink-600 hover:text-pink-600 mt-4 sm:mt-0" href="https://twitter.com/atridadl" target="_blank">
+        <vue-feather class="group-hover:text-pink-600" type="twitter"></vue-feather>
+        <div>Twitter</div>
+      </a>
+
+      <a class="text-center no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-pink-600 hover:text-pink-600 mt-4 sm:mt-0" href="https://www.instagram.com/atridadl/" target="_blank">
+        <vue-feather class="group-hover:text-pink-600" type="instagram"></vue-feather>
+        <div>Instagram</div>
+     </a>
     </div>
 
     <h3 class="text-2xl font-black text-center m-6">Freelance Software Engineer, Sysadmin, and Certified Nerd</h3>
