1ab65c5f39e0a14209faca1da878a1ebc24c76c3
Atridad Lahutton <88056492+atridadl@users.noreply.github.com>
2022-04-09 14:42:04 -0600

    Revert build

--- spa/vite.config.ts
+++ spa/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
   plugins: [
     vue(),
-    cssInjectedByJsPlugin()
   ]
 })
