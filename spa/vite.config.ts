5faf2ea9dbe64da17d2ca4cdf8d1cdc8516c8a2c
Atridad Lahutton <88056492+atridadl@users.noreply.github.com>
2022-04-09 14:17:16 -0600

    Updated SPA dependencies

--- spa/vite.config.ts
+++ spa/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import mdPlugin from 'vite-plugin-markdown';
 
 export default defineConfig({
   build: {
@@ -8,6 +7,5 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    mdPlugin()
   ]
 })
