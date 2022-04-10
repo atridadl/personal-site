021df220f5175ad9e38ceb0c8bc02afa692a0eb6
Atridad Lahutton <88056492+atridadl@users.noreply.github.com>
2022-04-10 00:45:05 -0600

    Wrong path

--- cdk/lib/blog.ts
+++ cdk/lib/blog.ts
@@ -94,7 +94,7 @@ export class BlogStack extends Stack {
 
     // ----------------------[S3 Bucket Deployment]----------------------
     new s3Deployment.BucketDeployment(this, `${ props.stage }-DeployBlogStatic`, {
-        sources: [s3Deployment.Source.asset("../blog/dist")],
+        sources: [s3Deployment.Source.asset("../blog/.vitepress/dist")],
         destinationBucket: bucket,
         distribution: cfDist,
         distributionPaths: [
