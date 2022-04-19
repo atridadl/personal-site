496ac79e0dbb8c56dde4cb925fff465279a88c70
Atridad Lahutton <88056492+atridadl@users.noreply.github.com>
2022-04-19 15:24:22 -0600

    Thanks co-pilot

--- serverless/functions/randomQuote.ts
+++ serverless/functions/randomQuote.ts
@@ -8,12 +8,36 @@ var params: ScanInput = {
     TableName: process.env.DBNAME || "",
 };
 
+const transformDynamoData  = (rawData: any) => {
+    if (!rawData || !!rawData.Items) {
+        return {
+            Items: [],
+        }
+    } else {
+        return {
+            Items: [...rawData.Items],
+        }
+    }
+};
 exports.main = async function randomQuote(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
     try {
-        const data = await dynamoClient.scan(params).promise();
+        const rawData = await dynamoClient.scan(params).promise();
+        const transformedData = transformDynamoData(rawData);
+
+        if (transformedData.Items.length > 0) {
+            const randomIndex = Math.floor(Math.random() * transformedData.Items.length);
+            const randomQuote = transformedData.Items[randomIndex];
+            return {
+                statusCode: 200,
+                body: JSON.stringify(randomQuote),
+            };
+        }
+
         return {
             statusCode: 200,
-            body: JSON.stringify(data)
+            body: JSON.stringify({
+                message: "No quotes found"
+            })
         }
     } catch (err) {
         return {
